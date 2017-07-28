b8b26b4328800c8fb430599e2a1f3d4a23fafbdc
waldinho <james.waller1984@gmail.com>
2017-07-28 16:00:53 +0100

    video.js depreciation fix.

---
+++ adsHaveEnded-NEW.js
@@ -0,0 +1,46 @@
+var registerPlugin = videojs.registerPlugin || videojs.plugin;
+
+registerPlugin('adsHaveEnded', function(options) {
+    var player = this;
+
+    function showOnAddEnd(options) {
+
+        var closeButton = document.getElementsByClassName("video-popout-close");
+
+        if (closeButton.length > 0) {
+            var closeButton = document.querySelector(".video-popout-close");
+
+            function fadeIn(options, closeButton) {
+                var closeButton = document.querySelector(".video-popout-close");
+                closeButton.removeAttribute("style")
+                closeButton.style.opacity = 0;
+                var fade = function() {
+                    closeButton.style.display = "table-cell";
+                    closeButton.style.position = "absolute";
+                    closeButton.style.bottom = "12px";
+                    closeButton.style.right = "0";
+                    closeButton.style.opacity = +closeButton.style.opacity + 0.01;
+                    if (+closeButton.style.opacity < 1) {
+                        (window.requestAnimationFrame && requestAnimationFrame(fade)) || setTimeout(fade, 16)
+                    }
+                };
+                fade();
+            }
+            fadeIn(closeButton);
+        } else {
+            console.log("WARNING: Close button not present");
+        }
+    };
+    player.on('ads-ad-ended', function(options) {
+        showOnAddEnd();
+    });
+    player.on('ima3-ad-error', function(options) {
+        showOnAddEnd();
+    });
+    player.on('ima3error', function(options) {
+        showOnAddEnd();
+    });
+    player.on('ima3-hard-timeout', function(options) {
+        showOnAddEnd();
+    });
+});
